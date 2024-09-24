ef874675f15622e40e075f375c705be94340ef41
Muhammad Waqas <mwaqasse27@gmail.com>
2024-09-24 10:49:28 +0500

    Update task2_lab4.cpp

--- task2_lab4.cpp
+++ task2_lab4.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 int main()
 {
-    
+   int search;
+    cout<<"Enter value to Search : ";
+    cin>>search;
+    bool found=false;
+
+    int numbers[10]={10,19,16,18,20,25,67,39,44,23};
+    for (int i = 0; i < 10; i++)
+    {
+        if (numbers[i] == search)
+        {
+            cout<<"*****Search Found***** "<<endl;
+            found=true;
+        }
+    }
+    if(found==false)
+    {
+        cout<<"*****Search not Found*****"<<endl;
+            
+    }
 }
